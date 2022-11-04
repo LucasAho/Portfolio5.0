e4c2d53723243ff8dd1825d5a85191edc8b5d9ca
Lucas's Laptop <aholucascode@gmail.com>
2022-11-04 16:32:17 -0600

    better tables

--- src/Components/Tukren/ArticleTable.js
+++ src/Components/Tukren/ArticleTable.js
@@ -14,52 +14,52 @@ const ArticleTable = () => {
             <TableContainer style={{ backgroundColor: '#AFA8BA' }} component={Paper}>
                 <Table size='small'>
                     <TableHead>
-                        <TableRow>
-                            <TableCell className='bbottom' scope="col"> </TableCell>
-                            <TableCell className='bleft btop bbottom' align='center' scope="col"><strong>Nom.</strong></TableCell>
-                            <TableCell className='bleft btop bbottom' align='center' scope="col"><strong>Acc.</strong></TableCell>
-                            <TableCell className='bleft btop bbottom bright' align='center' scope="col"><strong>Gen.</strong></TableCell>
-                            <TableCell className='bleft btop bbottom bright' align='center' scope="col"><strong>Dat.</strong></TableCell>
-                            <TableCell className='bleft btop bbottom bright' align='center' scope="col"><strong>Loc.</strong></TableCell>
-                            <TableCell className='bleft btop bbottom bright' align='center' scope="col"><strong>Instr.</strong></TableCell>
+                        <TableRow className='bbottom btop'>
+                            <TableCell scope="col"> </TableCell>
+                            <TableCell className='bleft' align='center' scope="col"><strong>Nom.</strong></TableCell>
+                            <TableCell className='bleft' align='center' scope="col"><strong>Acc.</strong></TableCell>
+                            <TableCell className='bleft bright' align='center' scope="col"><strong>Gen.</strong></TableCell>
+                            <TableCell className='bleft bright' align='center' scope="col"><strong>Dat.</strong></TableCell>
+                            <TableCell className='bleft bright' align='center' scope="col"><strong>Loc.</strong></TableCell>
+                            <TableCell className='bleft bright' align='center' scope="col"><strong>Instr.</strong></TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        <TableRow>
-                            <TableCell className='bleft bbottom btop' component='th' scope="row"><strong>Neutral</strong></TableCell>
-                            <TableCell className='bleft bbottom' align='center'>Di</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Dai</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Mi</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Dim</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Baf</TableCell>
-                            <TableCell className='bbottom bleft bright' align='center'>Nan</TableCell>
+                        <TableRow className='bbottom btop'>
+                            <TableCell className='bleft' component='th' scope="row"><strong>Neutral</strong></TableCell>
+                            <TableCell className='bleft' align='center'>Di</TableCell>
+                            <TableCell className='bleft' align='center'>Dai</TableCell>
+                            <TableCell className='bleft' align='center'>Mi</TableCell>
+                            <TableCell className='bleft' align='center'>Dim</TableCell>
+                            <TableCell className='bleft' align='center'>Baf</TableCell>
+                            <TableCell className='bleft bright' align='center'>Nan</TableCell>
                         </TableRow>
-                        <TableRow>
-                            <TableCell className='bleft bbottom btop' component='th' scope="row"><strong>Alien</strong></TableCell>
-                            <TableCell className='bleft bbottom' align='center'>Ku</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Kul</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Kim</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Kib</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Kab</TableCell>
-                            <TableCell className='bbottom bleft bright' align='center'>Kan</TableCell>
+                        <TableRow className='bbottom btop'>
+                            <TableCell className='bleft' component='th' scope="row"><strong>Alien</strong></TableCell>
+                            <TableCell className='bleft' align='center'>Ku</TableCell>
+                            <TableCell className='bleft' align='center'>Kul</TableCell>
+                            <TableCell className='bleft' align='center'>Kim</TableCell>
+                            <TableCell className='bleft' align='center'>Kib</TableCell>
+                            <TableCell className='bleft' align='center'>Kab</TableCell>
+                            <TableCell className='bleft bright' align='center'>Kan</TableCell>
                         </TableRow>
-                        <TableRow>
-                            <TableCell className='bleft bbottom btop' component='th' scope="row"><strong>Inanimate</strong></TableCell>
-                            <TableCell className='bleft bbottom' align='center'>Si</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Sau</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Sim</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Seb</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Saf</TableCell>
-                            <TableCell className='bbottom bleft bright' align='center'>Sun</TableCell>
+                        <TableRow className='bbottom btop'>
+                            <TableCell className='bleft' component='th' scope="row"><strong>Inanimate</strong></TableCell>
+                            <TableCell className='bleft' align='center'>Si</TableCell>
+                            <TableCell className='bleft' align='center'>Sau</TableCell>
+                            <TableCell className='bleft' align='center'>Sim</TableCell>
+                            <TableCell className='bleft' align='center'>Seb</TableCell>
+                            <TableCell className='bleft' align='center'>Saf</TableCell>
+                            <TableCell className='bleft bright' align='center'>Sun</TableCell>
                         </TableRow>
-                        <TableRow>
-                            <TableCell className='bleft bbottom btop' component='th' scope="row"><strong>Formal</strong></TableCell>
-                            <TableCell className='bleft bbottom' align='center'>O</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Do</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Dom</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Dokd</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Dei</TableCell>
-                            <TableCell className='bbottom bleft bright' align='center'>No</TableCell>
+                        <TableRow className='bbottom btop'>
+                            <TableCell className='bleft' component='th' scope="row"><strong>Formal</strong></TableCell>
+                            <TableCell className='bleft' align='center'>O</TableCell>
+                            <TableCell className='bleft' align='center'>Do</TableCell>
+                            <TableCell className='bleft' align='center'>Dom</TableCell>
+                            <TableCell className='bleft' align='center'>Dokd</TableCell>
+                            <TableCell className='bleft' align='center'>Dei</TableCell>
+                            <TableCell className='bleft bright' align='center'>No</TableCell>
                         </TableRow>
                     </TableBody>
                 </Table>
--- src/Components/Tukren/VerbTense.js
+++ src/Components/Tukren/VerbTense.js
@@ -14,49 +14,49 @@ const VerbTense = () => {
             <TableContainer style={{ backgroundColor: '#AFA8BA' }} component={Paper}>
                 <Table size='small'>
                     <TableHead>
-                        <TableRow>
-                            <TableCell className='bbottom' scope="col"> </TableCell>
-                            <TableCell className='bleft btop bbottom' align='center' scope="col"><strong>Perfective</strong></TableCell>
-                            <TableCell className='bleft btop bbottom' align='center' scope="col"><strong>Imperfective</strong></TableCell>
-                            <TableCell className='bleft btop bbottom bright' align='center' scope="col"><strong>Habitual</strong></TableCell>
+                        <TableRow className='bbottom btop'>
+                            <TableCell scope="col"></TableCell>
+                            <TableCell className='bleft' align='center' scope="col"><strong>Perfective</strong></TableCell>
+                            <TableCell className='bleft' align='center' scope="col"><strong>Imperfective</strong></TableCell>
+                            <TableCell className='bleft bright' align='center' scope="col"><strong>Habitual</strong></TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        <TableRow>
-                            <TableCell className='bleft bbottom btop' component='th' scope="row"><strong>Colony Past</strong></TableCell>
-                            <TableCell className='bleft bbottom' align='center'>Nodof</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Nodin</TableCell>
-                            <TableCell className='bbottom bleft bright' align='center'>Noduk</TableCell>
+                        <TableRow className='bbottom btop'>
+                            <TableCell className='bleft' component='th' scope="row"><strong>Colony Past</strong></TableCell>
+                            <TableCell className='bleft' align='center'>Nodof</TableCell>
+                            <TableCell className='bleft' align='center'>Nodin</TableCell>
+                            <TableCell className='bleft bright' align='center'>Noduk</TableCell>
                         </TableRow>
-                        <TableRow>
-                            <TableCell className='bleft bbottom btop' component='th' scope="row"><strong>Lifetime Past</strong></TableCell>
-                            <TableCell className='bleft bbottom' align='center'>Gof</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Binof</TableCell>
-                            <TableCell className='bbottom bleft bright' align='center'>Goden</TableCell>
+                        <TableRow className='bbottom btop'>
+                            <TableCell className='bleft' component='th' scope="row"><strong>Lifetime Past</strong></TableCell>
+                            <TableCell className='bleft' align='center'>Gof</TableCell>
+                            <TableCell className='bleft' align='center'>Binof</TableCell>
+                            <TableCell className='bleft bright' align='center'>Goden</TableCell>
                         </TableRow>
-                        <TableRow>
-                            <TableCell className='bleft bbottom btop' component='th' scope="row"><strong>Recent Past</strong></TableCell>
-                            <TableCell className='bleft bbottom' align='center'>Sahil</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Bin</TableCell>
-                            <TableCell className='bbottom bleft bright' align='center'>Sudan</TableCell>
+                        <TableRow className='bbottom btop'>
+                            <TableCell className='bleft' component='th' scope="row"><strong>Recent Past</strong></TableCell>
+                            <TableCell className='bleft' align='center'>Sahil</TableCell>
+                            <TableCell className='bleft' align='center'>Bin</TableCell>
+                            <TableCell className='bleft bright' align='center'>Sudan</TableCell>
                         </TableRow>
-                        <TableRow>
-                            <TableCell className='bleft bbottom btop' component='th' scope="row"><strong>Present</strong></TableCell>
-                            <TableCell className='bleft bbottom' align='center'>-</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>-</TableCell>
-                            <TableCell className='bbottom bleft bright' align='center'>-</TableCell>
+                        <TableRow className='bbottom btop'>
+                            <TableCell className='bleft' component='th' scope="row"><strong>Present</strong></TableCell>
+                            <TableCell className='bleft' align='center'>-</TableCell>
+                            <TableCell className='bleft' align='center'>-</TableCell>
+                            <TableCell className='bleft bright' align='center'>-</TableCell>
                         </TableRow>
-                        <TableRow>
-                            <TableCell className='bleft bbottom btop' component='th' scope="row"><strong>Future</strong></TableCell>
-                            <TableCell className='bleft bbottom' align='center'>Nat</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Nabi</TableCell>
-                            <TableCell className='bbottom bleft bright' align='center'>Nadun</TableCell>
+                        <TableRow className='bbottom btop'>
+                            <TableCell className='bleft' component='th' scope="row"><strong>Future</strong></TableCell>
+                            <TableCell className='bleft' align='center'>Nat</TableCell>
+                            <TableCell className='bleft' align='center'>Nabi</TableCell>
+                            <TableCell className='bleft bright' align='center'>Nadun</TableCell>
                         </TableRow>
-                        <TableRow>
-                            <TableCell className='bleft bbottom btop' component='th' scope="row"><strong>Colony Future</strong></TableCell>
-                            <TableCell className='bleft bbottom' align='center'>Kob</TableCell>
-                            <TableCell className='bbottom bleft' align='center'>Koben</TableCell>
-                            <TableCell className='bbottom bleft bright' align='center'>Dunekta</TableCell>
+                        <TableRow className='bbottom btop'>
+                            <TableCell className='bleft' component='th' scope="row"><strong>Colony Future</strong></TableCell>
+                            <TableCell className='bleft' align='center'>Kob</TableCell>
+                            <TableCell className='bleft' align='center'>Koben</TableCell>
+                            <TableCell className='bleft bright' align='center'>Dunekta</TableCell>
                         </TableRow>
                     </TableBody>
                 </Table>
