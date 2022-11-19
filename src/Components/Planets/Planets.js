8cdb1c159b3e6cb9af549f3bd6565301513045f2
Lucas's Laptop <aholucascode@gmail.com>
2022-11-18 17:40:31 -0700

    changed planet colors. reformatted page;

--- src/Components/Planets/Planets.js
+++ src/Components/Planets/Planets.js
@@ -49,8 +49,8 @@ const Planets = () => {
             <Typography ref={containerRef} variant="h3">Amondi-6 Solar System</Typography>
             <Typography variant="body1">The host star of the planet Amondi-6b was named Amondi-6 by the humans who discovered and inhabited it. In total, 11 planets were discovered orbiting this star. The star and 6b were selected for their similarities to humanity's own Sun and Earth, in order to reduce operating costs on the planet.</Typography>
 
-            <Typography variant="h4">The Naked Eye Planets of Amondi-6</Typography>
-            <Typography variant="body1">Only 6 of the 11 planets have been discovered by the inhabitants of Maalima. Each culture has its own names and beliefs around these planets.</Typography>
+
+            <Typography variant="body1">Only 6 of the 11 planets have been discovered by the inhabitants of Maalima. Each culture has its own names and beliefs around these planets. Many rohīren believe that the 6 planets were scultped and placed by Radren, as reminders of the 6 pillars. As such, each planet is named after its corresponding pillar. The moon is their guiding light through the pillars.</Typography>
             <TableContainer style={{ marginBottom: '1rem' }} component={Paper}>
                 <Table size='small'>
                     <TableHead>
@@ -94,8 +94,8 @@ const Planets = () => {
                         </TableRow>
                         <TableRow className='bbottom btop'>
                             <TableCell className='bleft' component='th' scope="row">6</TableCell>
-                            <TableCell className='bleft' align='center'>Amondi-6k</TableCell>
-                            <TableCell className='bleft' align='center'>Lis Nocātek</TableCell>
+                            <TableCell className='bleft' align='center'>Amondi-6o</TableCell>
+                            <TableCell className='bleft' align='center'>Lis Nacātek</TableCell>
                             <TableCell className='bleft bright' align='center'>-</TableCell>
                         </TableRow>
                         <TableRow className='bbottom btop'>
@@ -104,31 +104,55 @@ const Planets = () => {
                             <TableCell className='bleft' align='center'>Radrog</TableCell>
                             <TableCell className='bleft bright' align='center'>-</TableCell>
                         </TableRow>
+                        <TableRow className='bbottom btop'>
+                            <TableCell className='bleft' component='th' scope="row">8</TableCell>
+                            <TableCell className='bleft' align='center'>Amondi-6k</TableCell>
+                            <TableCell className='bleft' align='center'>-</TableCell>
+                            <TableCell className='bleft bright' align='center'>-</TableCell>
+                        </TableRow>
+                        <TableRow className='bbottom btop'>
+                            <TableCell className='bleft' component='th' scope="row">9</TableCell>
+                            <TableCell className='bleft' align='center'>Amondi-6l</TableCell>
+                            <TableCell className='bleft' align='center'>-</TableCell>
+                            <TableCell className='bleft bright' align='center'>-</TableCell>
+                        </TableRow>
+                        <TableRow className='bbottom btop'>
+                            <TableCell className='bleft' component='th' scope="row">10</TableCell>
+                            <TableCell className='bleft' align='center'>Amondi-6m</TableCell>
+                            <TableCell className='bleft' align='center'>-</TableCell>
+                            <TableCell className='bleft bright' align='center'>-</TableCell>
+                        </TableRow>
+                        <TableRow className='bbottom btop'>
+                            <TableCell className='bleft' component='th' scope="row">11</TableCell>
+                            <TableCell className='bleft' align='center'>Amondi-6n</TableCell>
+                            <TableCell className='bleft' align='center'>-</TableCell>
+                            <TableCell className='bleft bright' align='center'>-</TableCell>
+                        </TableRow>
                     </TableBody>
                 </Table>
             </TableContainer>
+            <Typography variant="h4">The Naked Eye Planets of Amondi-6</Typography>
             <Typography variant="caption">Distance and orbital period are to scale. Planet/star size is not.</Typography>
             <Stage style={{ backgroundColor: 'black', overflow: 'hidden' }} width={screenWidth} height={screenHeight}>
                 <Layer>
                     {/* Sun // Amondi-6 */}
-                    <Circle x={screenWidth / 2} y={(screenHeight / 2)} radius={50} fill="yellow" />
+                    <Circle x={screenWidth / 2} y={(screenHeight / 2)} radius={50} fill="GoldenRod" />
                     {/* Amondi-6c //  Halir */}
-                    <Circle ref={halir} x={(screenWidth / 2) - 19} y={screenHeight / 2} distance={94} year={0.08} radius={4} fill="brown" />
+                    <Circle ref={halir} x={(screenWidth / 2) - 19} y={screenHeight / 2} distance={94} year={0.08} radius={4} fill="SlateBlue" />
                     {/* Amondi-6d // Hibakhla */}
-                    <Circle ref={hibakhla} x={(screenWidth / 2) - 38} y={screenHeight / 2} distance={113} year={0.23} radius={7} fill="brown" />
+                    <Circle ref={hibakhla} x={(screenWidth / 2) - 38} y={screenHeight / 2} distance={113} year={0.23} radius={7} fill="SaddleBrown" />
                     {/* Amondi-6e // Hihaihli */}
                     <Circle ref={hihaihli} x={(screenWidth / 2) - 64} y={screenHeight / 2} distance={139} year={0.51} radius={9} fill="brown" />
                     {/* Amondi-6b // Relya // Maalima */}
                     <Circle ref={relya} x={(screenWidth / 2) - 98} y={screenHeight / 2} distance={173} year={0.97} radius={11} fill="green" />
                     {/* Amondi-6f // Himuta */}
-                    <Circle ref={himuta} x={(screenWidth / 2) - 167} y={screenHeight / 2} distance={242} year={2.16} radius={10} fill="red" />
+                    <Circle ref={himuta} x={(screenWidth / 2) - 167} y={screenHeight / 2} distance={242} year={2.16} radius={10} fill="Maroon" />
                     {/* Amondi-6k // Lis Nacatek */}
                     <Circle ref={nacatek} x={(screenWidth / 2) - 261} y={screenHeight / 2} distance={336} year={4.22} radius={3} fill="purple" />
                     {/* Amondi-6a // Radrog */}
                     <Circle ref={radrog} x={(screenWidth / 2) - 476} y={screenHeight / 2} distance={551} year={10.39} radius={25} fill="orange" />
                 </Layer>
             </Stage>
-
         </Container>
     );
 };
